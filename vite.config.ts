a422fef92d3cc2fa5ef9023e3b62d700223ae8f5
sharof-dev <sharofbekmardiyev@gmail.com>
2024-12-23 02:46:26 +0500

    sdfsf

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png'],
-  base: '/food-app-bot/',
   css: {
     modules: {
       localsConvention: "camelCase"
