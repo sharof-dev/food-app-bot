ef0e860d131636eacac0e67ab98f641fb2f4cf6c
sharof-dev <sharofbekmardiyev@gmail.com>
2024-12-24 21:51:01 +0500

    jgfh

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ const App = () => {
     void cwindow?.Telegram?.WebApp.ready();
     void cwindow?.Telegram?.WebApp.expand(true);
     void cwindow?.Telegram?.WebApp.enableClosingConfirmation();
+    void cwindow?.Telegram?.WebApp.setHeaderColor("#1A202E");
+    void cwindow?.Telegram?.WebApp.setBackgroundColor("#1A202E");
+    void cwindow?.Telegram?.WebApp.disableVerticalSwipes();
   }, [])
   return (
     <div className="bg-[#1e1e1e] font-body">
