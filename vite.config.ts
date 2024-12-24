e82e4de7b32bd57fdfb8099a74175330df17576f
sharof-dev <sharofbekmardiyev@gmail.com>
2024-12-24 21:45:15 +0500

    jhfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/food-app-bot/',
+  // base: '/food-app-bot/',
   assetsInclude: ['**/*.png'],
   css: {
     modules: {
